ab3b1be7dc0081b517f19e08e4a3449a555ec943
tapuks <david_berdiell@hotmail.com>
2021-05-17 19:25:14 +0000

    styles

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useState } from "react";
+<script
+	src="https://kit.fontawesome.com/b675d79ae8.js"
+	crossorigin="anonymous"></script>;
 
 const ToDoList = () => {
-	const [todos, setTodos] = useState([
-		{ title: "hola", id: 1 },
-		{ title: "adios", id: 2 }
-	]);
-
+	const [todos, setTodos] = useState([{ title: "yo", id: 2 }]);
 	const [task, setTask] = useState("");
 
 	const addTask = () => {
-		const newTodos = todos.concat({ title: task, id: todos.length + 1 });
-		setTodos(newTodos);
+		if (task != "") {
+			const newTodos = todos.concat({
+				title: task,
+				id: todos.length + 1
+			});
+			setTodos(newTodos);
+		}
 	};
 
 	const deleteTask = todoId => {
@@ -22,25 +26,39 @@ const ToDoList = () => {
 
 	return (
 		<div>
-			<input
-				type="text"
-				value={task}
-				onChange={e => setTask(e.target.value)}
-			/>
-			<button onClick={addTask}>Add</button>
+			<div className="contain">
+				<h1 className="title">todos</h1>
+				<div className="contain-input">
+					<input
+						type="text"
+						value={task}
+						placeholder="Whats needs to be done"
+						onChange={e => setTask(e.target.value)}
+					/>
+					<button className="btn btn-primary" onClick={addTask}>
+						Add
+					</button>
+				</div>
 
-			<ul>
-				{todos.map(todo => {
-					return (
-						<li key={todo.id}>
-							{todo.title}
-							<button onClick={() => deleteTask(todo.id)}>
-								Delete
-							</button>
-						</li>
-					);
-				})}
-			</ul>
+				<div className="contain-ul">
+					<ul>
+						{todos.map(todo => {
+							return (
+								<li key={todo.id}>
+									<span className="li-text">
+										{todo.title}
+									</span>
+									<span
+										className="button-delete"
+										onClick={() => deleteTask(todo.id)}>
+										<i class="fas fa-times button-delete"></i>
+									</span>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
